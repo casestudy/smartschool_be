a19fe28ee006c1e63c640c882083218cf18777be
Femencha Azombo Fabrice <femenchaazombofabrice@gmail.com>
2024-03-30 16:28:57 +0100

    Updated to fix issue with printing class list (#6)

--- be-pdf/src/index.ts
+++ be-pdf/src/index.ts
@@ -6,7 +6,7 @@ import createStudentReceiptTemplate from "./templates/studentreceipts";
 const app = express();
 app.use(express.json());
 
-const port = 5000;
+const port = 6000;
 app.listen(port, () => {
   console.log(`The sample PDF app is running on port ${port}.`);
 });
@@ -15,14 +15,21 @@ app.listen(port, () => {
 app.post("/classlist", async (req, res) => {
     // Calling the template render func with dynamic data
     const data = JSON.parse(req.body.data);
+    const cname = data.details.replace(/ /g, "_");
+    let currentyear = data.year.replace("/","-");
     const result = await createClassListTemplate(data);
+
+    if(!fs.existsSync(`../be/uploads/classlists/${currentyear}`)) {
+      fs.mkdirSync(`../be/uploads/classlists/${currentyear}`, { recursive: true });
+    }
   
     // Streaming our resulting pdf back to the user
-    let f = result.pipe(fs.createWriteStream(`../be/uploads/classlists/${data.details}.pdf`));
+    let f = result.pipe(fs.createWriteStream(`../be/uploads/classlists/${currentyear}/${cname}.pdf`));
     f.on('finish', function() {
       res.send('{"error":false}');
     });
-    f.on('error', function() {
+    f.on('error', function(err) {
+      console.log(err);
       res.send('{"error": true, "msg":"Could not write stream"}');
     });    
 });
